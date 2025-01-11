3d66f20c49c83a8b6e29a42148f2e1389b3c498e
Sameer Khichi <sameerrkhichi@gmail.com>
2025-01-11 16:24:46 -0500

    Refactoring code to implement GUI, Command line merger in seperate file, created a basic window using tkinter

--- merger.py
+++ merger.py
@@ -1,30 +1,45 @@
-from PyPDF2 import PdfWriter
-import os
+#this merger uses tkinter to implement a GUi
+import tkinter as tk
+from tkinter import filedialog
 
-#creating a instance of the pdfwriter object
-merger = PdfWriter()
+#making the main window
+root = tk.Tk()
+root.title("PDF Merger")
+root.geometry('800x600')
 
-#storing the current path
-original_path = os.getcwd()
+#This is an array to hold the selected files
+files_to_merge = []
 
-#changing directory to the folder with the pdf files
-os.chdir('files_to_merge')
+#creates a file box to display the selected files
+file_list = tk.Listbox(root, width=50, height=10)
+file_list.pack(pady=10)
 
-#storing the new directory
-current_path = os.getcwd()
 
-#for each pdf in the folder of files, append it to the merger
-for file in os.listdir(current_path):
-    merger.append(file)
+def select_files():
+    
+    #using the global variable, asking user to select their files they want to merge
+    global files_to_merge
+    files = filedialog.askopenfilenames(title="Select PDF Files", filetypes=[("PDF files", "*.pdf")])
 
-#switching the working path back to the original one
-os.chdir(original_path)
+    if files:
+        files_to_merge = list(files)
+        update_file_list()
+    
 
-#writing the output to a separate pdf file
-#note that the wb type will create a file if it doesn't already exist
-output_pdf = open("merged_pdf.pdf", "wb")
-merger.write(output_pdf)
+#this is a function to display the user selected files
+def update_file_list():
 
-#closing the files at the end
-merger.close()
-output_pdf.close()
+    #clears the box for the list
+    file_list.delete(0, tk.END)
+
+    #inserts each file selected
+    for file in files_to_merge:
+        file_list.insert(tk.END, file)
+
+
+#this is making the button to select the files
+select_button = tk.Button(root, text = "Select PDFs", command = select_files)
+select_button.pack(pady=10)
+
+#running the main loop
+root.mainloop()
---
+++ mergerCL.py
@@ -0,0 +1,31 @@
+#THis is a merger that works from the command line
+from PyPDF2 import PdfWriter
+import os
+
+#creating a instance of the pdfwriter object
+merger = PdfWriter()
+
+#storing the current path
+original_path = os.getcwd()
+
+#changing directory to the folder with the pdf files
+os.chdir('files_to_merge')
+
+#storing the new directory
+current_path = os.getcwd()
+
+#for each pdf in the folder of files, append it to the merger
+for file in os.listdir(current_path):
+    merger.append(file)
+
+#switching the working path back to the original one
+os.chdir(original_path)
+
+#writing the output to a separate pdf file
+#note that the wb type will create a file if it doesn't already exist
+output_pdf = open("merged_pdf.pdf", "wb")
+merger.write(output_pdf)
+
+#closing the files at the end
+merger.close()
+output_pdf.close()
